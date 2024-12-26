ffb2b1b226bdb612889f4c51a4b06bbb033dc763
yinxianwei <yinxianwei@qq.com>
2024-12-26 17:53:58 +0800

    get position

--- web/views/Index.vue
+++ web/views/Index.vue
@@ -1,5 +1,24 @@
 <template>
-    <div>index</div>
+    <div>
+        <van-cell-group inset title=" ">
+            <van-cell title="获取定位" is-link @click="getCurrentPosition" />
+        </van-cell-group>
+    </div>
 </template>
-<script setup></script>
+<script setup>
+import { showDialog } from 'vant';
+function getCurrentPosition() {
+    window.harmony.getCurrentPosition(
+        (res) => {
+            showDialog({ message: JSON.stringify(res) });
+        },
+        (err) => {
+            console.error(err);
+        },
+        {
+            timeout: 5000,
+        }
+    );
+}
+</script>
 <style></style>
